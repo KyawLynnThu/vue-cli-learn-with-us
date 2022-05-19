150251404b8e031a7544530125814165a0223ca2
Kyaw Lin Thu <scm.kyawlinthu@gmail.com>
2022-05-19 16:20:23 +0630

    home upload

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row align-items-center mt-5"> 
+        <div class="col-12 col-md-8 p-0">
+          <div class=" text-md-right text-center p-md-5">
+            <h3 class="font-weight-bold">Become an instructor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Ratione amet libero pariatur, mollitia enim maxime! Perspiciatis, 
+              provident earum. Temporibus placeat possimus obcaecati 
+              accusantium fuga earum, magni qui facilis porro exercitationem!
+            </p>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="p-md-0 pt-3 text-center">
+            <img src="../assets/logo.png" alt="" class="w-65">
+          </div>
+        </div>
+      </div>
+</template>
