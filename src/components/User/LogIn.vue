c3399f56e8e7bdad5e623acc5dcbd01b316294b5
maythainthin <nanthazinmay@gmail.com>
2022-05-26 11:03:40 +0630

    login signup confirmSignup modify issues after review

--- src/components/User/LogIn.vue
+++ src/components/User/LogIn.vue
@@ -1,65 +1,83 @@
 <template>
-<div class="row">
-  <div class="col-lg-4 col-md-6 mx-auto mt-5 pt-5 col-12">
-    <h2 class="text-center m-3">LOG IN</h2>
-    <form class="bg-light p-4">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input v-model="loginData.email" type="email" class="form-control" required />
-      </div>
-      <div class="form-group">
-        <label for="">Password</label>
-        <input v-model="loginData.password" type="password" class="form-control" required />
-      </div>
-      <div class="mb-3 mt-3">
-        <button @click="login" type="submit" class="
+  <div class = "row">
+    <div class = "col-lg-4 col-md-6 mx-auto mt-5 pt-5 col-12">
+      <h2 class = "text-center m-3">LOG IN</h2>
+      <form class = "bg-light p-4">
+        <div class = "form-group">
+          <label for = "email">Email</label>
+          <input
+            v-model = "loginData.email"
+            type = "email"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class = "form-group">
+          <label for = "">Password</label>
+          <input
+            v-model = "loginData.password"
+            type = "password"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class = "mb-3 mt-3">
+          <button
+            @click = "login"
+            type = "submit"
+            class = "
               login-btn
               btn btn-dark btn-block
               text-uppercase
               font-weight-bold
-            ">
-          log in
-        </button>
-      </div>
-      <div class="mb-3">
-        <router-link :to="{ name: 'SignUp' }" type="submit" class="
+            "
+          >
+            log in
+          </button>
+        </div>
+        <div class = "mb-3">
+          <router-link
+            :to = "{ name: 'SignUp' }"
+            type = "submit"
+            class = "
               btn btn-outline-dark btn-block
               text-uppercase
               mb-3
               font-weight-bold
-            ">
-          Register
-        </router-link>
-      </div>
-      <div class="text-center mt-1">
-        <a class="text-decoration-none text-dark">Forgot Password?</a>
-      </div>
-    </form>
+            "
+          >
+            Register
+          </router-link>
+        </div>
+        <div class = "text-center mt-1">
+          <a class = "text-decoration-none text-dark">Forgot Password?</a>
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "LogIn",
   data() {
     return {
       loginData: {
-        email: '',
-        password: ''
-      }
-    }
+        email: "",
+        password: "",
+      },
+    };
   },
   methods: {
     login() {
-      axios.post('http://127.0.0.1:8000/api/user/login', this.login)
+      axios.post("http://127.0.0.1:8000/api/user/login", this.login)
         .then(res => {
           let token = res.data.user.api_token;
-          localStorage.setItem('loginToken', token);
-        })
-    }
-  }
+          localStorage.setItem("loginToken", token);
+        });
+    },
+  },
 };
 </script>
 
--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,41 +1,70 @@
 <template>
-<div class="row confirm">
-  <div class="col-lg-4 col-md-6 col-12 mx-auto">
-    <h2 class="text-center m-3 text-uppercase">Sign Up</h2>
-    <form class="bg-light p-4">
-      <div class="form-group">
-        <label for="name">Username</label>
-        <input v-model="registerData.name" type="text" class="form-control" required />
-      </div>
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input v-model="registerData.email" type="email" class="form-control" required />
-      </div>
-      <div class="form-group">
-        <label for="">Password</label>
-        <input v-model="registerData.password" type="password" class="form-control" required />
-      </div>
-      <div class="form-group">
-        <label for="">Confirm Password</label>
-        <input v-model="registerData.password_comfirm" type="password" class="form-control" required />
-      </div>
-      <div class="mb-3 mt-3">
-        <router-link @click="register" :to="{ name: 'confirmSignup' }" type="submit" class="btn btn-dark btn-block text-uppercase font-weight-bold">
-          Register
-        </router-link>
-      </div>
-      <div>
-        <router-link :to="{ name: 'LogIn' }" type="submit" class="
+  <div class = "row confirm">
+    <div class = "col-lg-4 col-md-6 col-12 mx-auto">
+      <h2 class = "text-center m-3 text-uppercase">Sign Up</h2>
+      <form class = "bg-light p-4">
+        <div class = "form-group">
+          <label for = "name">Username</label>
+          <input
+            v-model = "registerData.name"
+            type = "text"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class = "form-group">
+          <label for= "email">Email</label>
+          <input
+            v-model = "registerData.email"
+            type = "email"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class = "form-group">
+          <label for = "">Password</label>
+          <input
+            v-mode l= "registerData.password"
+            type = "password"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class = "form-group">
+          <label for ="">Confirm Password</label>
+          <input
+            v-model = "registerData.password_comfirm"
+            type = "password"
+            class = "form-control"
+            required
+          />
+        </div>
+        <div class="mb-3 mt-3">
+          <router-link
+            @click = "register"
+            :to = "{ name: 'confirmSignup' }"
+            type = "submit"
+            class = "btn btn-dark btn-block text-uppercase font-weight-bold"
+          >
+            Register
+          </router-link>
+        </div>
+        <div>
+          <router-link
+            :to = "{ name: 'LogIn' }"
+            type = "submit"
+            class = "
               btn btn-outline-dark btn-block
               text-uppercase
               font-weight-bold
-            ">
-          log in
-        </router-link>
-      </div>
-    </form>
+            "
+          >
+            log in
+          </router-link>
+        </div>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -54,13 +83,12 @@ export default {
   },
   methods: {
     register() {
-      axios
-        .post("http://127.0.0.1:8000/api/user/register", this.registerData)
-        .then((res) => {
+      axios.post("http://127.0.0.1:8000/api/user/register", this.registerData)
+        .then(res => {
           let token = res.data.api_token;
           localStorage.setItem("token", token);
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
